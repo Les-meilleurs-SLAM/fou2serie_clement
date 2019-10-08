a9639f4af651c60800138b7c156ff5ee72bc091e
cdudit <clement.dudit85@gmail.com>
2019-10-08 11:53:20 +0200

    tout ok + à jour TP

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -9,24 +9,44 @@ use Symfony\Component\HttpFoundation\Response;
 
 class GenreController extends AbstractController
 {
-    /**
-     * @Route("/genre", name="genre")
-     */
-    public function index()
-    {
-        /*//Liste des genres à ajouter
-        $noms = array('drame', 'comédie', 'comique', 'fantastique', 'horreur', 'thriller', 'policier', 'suspens', 'documentaire', 'action');
-        foreach ($noms as $i => $nom) {
-            // on crée un tableau avec les objets Genre
-            $newGenre = new Genre();
-            $liste_genre[$i] = $newGenre;
-            $liste_genre[$i]->setLibelleGenre($nom);
-            // On persiste la liste
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($liste_genre[$i]);
-        }
-        // On déclenche l'enregistrement
-        $entityManager->flush();
-        return new Response("Ajout effectué.");*/
-    }
+    // /**
+    //  * @Route("/genre", name="genre")
+    //  */
+    // public function index()
+    // {
+    //     //Liste des genres à ajouter
+    //     $noms = array('drame', 'comédie', 'comique', 'fantastique', 'horreur', 'thriller', 'policier', 'suspens', 'documentaire', 'action');
+    //     foreach ($noms as $i => $nom) {
+    //         // on crée un tableau avec les objets Genre
+    //         $newGenre = new Genre();
+    //         $liste_genre[$i] = $newGenre;
+    //         $liste_genre[$i]->setLibelleGenre($nom);
+    //         // On persiste la liste
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($liste_genre[$i]);
+    //     }
+    //     // On déclenche l'enregistrement
+    //     $entityManager->flush();
+    //     return new Response("Ajout effectué.");
+    // }
+    
+    // /**
+    //  * @Route("/loadGenres", name="loadGenres")
+    //  */
+    // public function loadGenres()
+    // {
+    //     //Liste des genres à ajouter
+    //     $noms = array('action', 'policier', 'aventure');
+    //     $entityManager = $this->getDoctrine()->getManager();
+    //     foreach ($noms as $i => $nom) {
+    //         // on crée un tableau avec les objets Genre
+    //         $liste_genre[$i] = new Genre();
+    //         $liste_genre[$i]->setLibelle($nom);
+    //         // On persiste la liste
+    //         $entityManager->persist($liste_genre[$i]);
+    //     }
+    //     // On déclenche l'enregistrement
+    //     $entityManager->flush();
+    //     return new Response("ajout effectué");
+    // }
 }
